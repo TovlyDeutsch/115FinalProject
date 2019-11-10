676649d8631ca303a5226f088459c8234c1856f0
Tovly Deutsch <tovlydeutsch@gmail.com>
2019-11-09 20:46:05 -0500

    started adding support for constraints

--- data.py
+++ data.py
@@ -16,29 +16,40 @@ import random
 import math
 import time
 
+class Sampleable():
+    def __init__(self, choices: List[Tuple[int, float]]):
+      possible_segments = [x[0] for x in choices]
+      probs = [x[1] for x in choices]
+      self.dist = stats.rv_discrete(name='custm', values=(possible_segments, probs))
+    
+    @abstractmethod
+    def sample(self) -> str:
+      pass
 
-class Segment():
-  def __init__(self, choices: List[Tuple[int, float]]):
-    possible_segments = [x[0] for x in choices]
-    probs = [x[1] for x in choices]
-    self.dist = stats.rv_discrete(name='custm', values=(possible_segments, probs))
-  
+class Segment(Sampleable):
   def sample(self):
     phone_lookup = {
-      0: ' ', 1: 'a', 2: 'k'
+      0: '<sep>', 1: 'a', 2: 'k'
     }
     return phone_lookup[self.dist.rvs()]
 
-class Sequence():
-  def __init__(self):
-    pass
+class Constraint(Sampleable):
+  def sample(self):
+    con_lookup = {
+      0: '<sep>', 1: 'agree', 2: '*D'
+    }
+    return con_lookup[self.dist.rvs()]
+
+# class Sequence():
+#   def __init__(self):
+#     pass
 
 class Example():
   def __init__(self, src, trg):
     self.src = src
     self.trg = trg
 
-def gen_src(segments: List[Segment]):
+def gen_seq(segments: Sequence[Sampleable]):
   src = ['<sos>']
   number_of_outputs = randint(5, 15)
   for i in range(number_of_outputs):
@@ -49,27 +60,17 @@ def gen_src(segments: List[Segment]):
   src.append('<eos>')
   return src
 
-def gen_ranking(segments: List[Segment]):
-  src = ['<sos>']
-  number_of_outputs = randint(5, 15)
-  for i in range(number_of_outputs):
-    for seg in segments:
-      src.append(seg.sample())
-    if i != number_of_outputs - 1:
-      src.append('<sep>')
-  src.append('<eos>')
-  return src
-
-def gen_example(segments: List[Segment], rankings):
-  example = Example(gen_src(segments), None)
+def gen_example(segments: List[Segment], constraints: List[Constraint]):
+  example = Example(gen_seq(segments), gen_seq(constraints))
   return example
 
 
 if __name__ == "__main__":
   test_seg = [(1, 0.8), (2, 0.2)]
-  train_data = [gen_example(test_seg, None) for i in range(10)]
-  valid_data = [gen_example(test_seg, None) for i in range(10)]
-  test_data = [gen_example(test_seg, None) for i in range(10)]
+  test_ranking = [(1, 0.8), (2, 0.2)]
+  train_data = [gen_example(test_seg, test_ranking) for i in range(10)]
+  valid_data = [gen_example(test_seg, test_ranking) for i in range(10)]
+  test_data = [gen_example(test_seg, test_ranking) for i in range(10)]
   SRC = Field()
   TRG = Field()
 
