d53c79af3208aa46d7a55dbc64692ebcd0dd5025
Tovly Deutsch <tovlydeutsch@gmail.com>
2019-11-14 21:12:01 -0500

    made types more clear

--- data.py
+++ data.py
@@ -20,12 +20,42 @@ from abc import ABC, abstractmethod
 
 
 class Sampleable(ABC):
-  def __init__(self, choices: List[Tuple[str, float]]):
+  def __init__(self, choices: List[Tuple[Any, float]]):
     self.possible_segments = [x[0] for x in choices]
     self.probs = [x[1] for x in choices]
 
   def sample(self) -> str:
     return np.random.choice(self.possible_segments, 1, p=self.probs)[0]
+
+
+Constraint = str
+Ranking = List[Constraint]
+
+
+class PossibleRankings(Sampleable):
+  def __init__(self, choices: List[Tuple[Ranking, float]]):
+    super().__init__(choices)
+
+
+class PossibleSegments(Sampleable):
+  def __init__(self, choices: List[Tuple[str, float]]):
+    super().__init__(choices)
+
+
+class Word():
+  def __init__(self, segments: List[PossibleSegments]):
+    self.segments = segments
+
+  def gen_sample_output(self):
+    """This generates multiple intances of a word/input seperated by <sep>"""
+    src = ['<sos>']
+    number_of_outputs = randint(5, 15)  # TODO remove magic numbers here
+    for i in range(number_of_outputs):
+      for seg in self.segments:
+        src.append(seg.sample())
+        src.append('<sep>')
+    src[-1] = '<eos>'
+    return src
 
 
 class Example():
@@ -34,61 +64,44 @@ class Example():
     self.trg = trg
 
 
-def gen_seq(segments: Sequence[Sampleable]):
-  """This generates multiple intances of a word/input seperated by <sep>"""
-  src = ['<sos>']
-  number_of_outputs = randint(5, 15)  # TODO remove magic numbers here
-  for i in range(number_of_outputs):
-    for seg in segments:
-      src.append(seg.sample())
-      src.append('<sep>')
-  src[-1] = '<eos>'
-  return src
-
-
-def gen_example(segments: List[Sampleable], constraints: List[Sampleable]):
-  example = Example(gen_seq(segments), gen_seq(constraints))
-  return example
-
-
-def gen_examples_for_input(
-        word: List[Sampleable],
-        ranking: List[Sampleable],
+def gen_examples_for_word_and_rankings(
+        word: Word,
+        rankings: PossibleRankings,
         min_num: int,
         max_num: int):
-  # TODO maybe change from unifrom random to dome dist weight toward larger
-  # nums
+  # TODO maybe change from uniform random to dome dist weight toward larger
   num_examples = randint(min_num, max_num)
-  return [gen_example(word, ranking) for i in range(num_examples)]
+  return [Example(word.gen_sample_output(), rankings.sample())
+          for i in range(num_examples)]
 
 
 def gen_all_examples(
-        words_and_rankings: List[Tuple[List[Sampleable], List[Sampleable]]]):
+        words_and_rankings: List[Tuple[Word, PossibleRankings]]):
   examples = []
-  min_number_of_examples = 20
+  min_number_of_examples = 20  # TODO maybe make this command line arg
   max_number_of_examples = 100
-  for word, ranking in words_and_rankings:
-    examples += gen_examples_for_input(word,
-                                       ranking,
-                                       min_number_of_examples,
-                                       max_number_of_examples)
+  for word, rankings in words_and_rankings:
+    examples += gen_examples_for_word_and_rankings(word,
+                                                   rankings,
+                                                   min_number_of_examples,
+                                                   max_number_of_examples)
   return examples
 
 
 def seg(char: str):
-  return Sampleable([(char, 1.0)])
+  return PossibleSegments([(char, 1.0)])
 
 
 def u_seg(chars: List[str]):
-  return [(char, 1 / len(chars)) for char in chars]
+  return PossibleSegments([(char, 1 / len(chars)) for char in chars])
 
 
 if __name__ == "__main__":
   # start English z suffix devoicing examples
   consonant = u_seg(['k', 'd', 'h', 't', 'ʔ', 'w'])
   vowel = u_seg(['æ', 'ɔ', 'ɛ', 'e', 'I'])
-  s_z_seg = Sampleable([('s', 0.8), ('z', 0.2)])
-  f_v_seg = Sampleable([('f', 0.8), ('v', 0.2)])
+  s_z_seg = PossibleSegments([('s', 0.8), ('z', 0.2)])
+  f_v_seg = PossibleSegments([('f', 0.8), ('v', 0.2)])
 
   cat_sz = [consonant, vowel, seg('t'), s_z_seg]
   gz = [consonant, vowel, seg('g'), seg('z')]
@@ -99,7 +112,7 @@ if __name__ == "__main__":
   one_vowel_t_theta = [consonant, vowel, seg('t'), seg('θ')]
   n_theta = [consonant, vowel, seg('n'), seg('θ')]
 
-  end_voi_words = [
+  end_voi_words = list(map(Word, [
       cat_sz,
       gz,
       nz,
@@ -107,14 +120,15 @@ if __name__ == "__main__":
       four_f_v_th,
       two_vowel_t_theta,
       one_vowel_t_theta,
-      n_theta]
+      n_theta]))
 
   agree, ident_voi, star_d, star_d_sigma = 'Agree', '*Ident-IO(voi)', '*D', '*D_sigma'
-  english_voi = [agree, ident_voi, star_d, star_d_sigma]
-  non_english_voi = [ident_voi, agree, star_d, star_d_sigma]
-  voi_rankings = [(english_voi, 0.8), (non_english_voi, 0.2)]
+  english_voi: Ranking = [agree, ident_voi, star_d, star_d_sigma]
+  non_english_voi: Ranking = [ident_voi, agree, star_d, star_d_sigma]
+  voi_rankings = PossibleRankings([(english_voi, 0.8), (non_english_voi, 0.2)])
+
   end_voi_examples = [(word, voi_rankings) for word in end_voi_words]
-  # start English z suffix devoicing examples
+  # end English z suffix devoicing examples
 
   words_and_rankings = end_voi_examples  # TODO will add more concantenated here
 
@@ -135,9 +149,3 @@ if __name__ == "__main__":
       (train_data, valid_data, test_data),
       batch_size=BATCH_SIZE,
       device=device)
-
-  # segs = []
-  # seg = Sampleable([(1, 0.8), (2, 0.2)])
-  # for i in range(10):
-  #   segs.append(seg.sample())
-  # print(segs)
